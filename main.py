b6fcc700972263781e72601753f0d3cd01b858e0
Tapochekmira <85840326+Tapochekmira@users.noreply.github.com>
2022-02-16 12:49:42 +0300

    download book covers.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from pathlib import Path
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlparse
 
 
 def check_for_redirect(response):
@@ -27,7 +28,34 @@ def download_txt(response, filename, folder='books/'):
         file.write(response.content)
 
 
-def download_books(folder):
+def get_book_image_url(book_id):
+    url = f'http://tululu.org/b{book_id}'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    image_tag = soup.find('td', class_='ow_px_td').find('img')
+    image_url = image_tag['src']
+    image_url = urljoin('http://tululu.org/', image_url)
+    return image_url
+
+
+def get_image_name(book_image_url):
+    image_path = urlparse(book_image_url).path
+    image_name = image_path.split('/')[-1]
+    return image_name
+
+
+def download_image(url, image_name, folder='image/'):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    directory = os.path.join(folder, image_name)
+    with open(directory, 'wb') as file:
+        file.write(response.content)
+
+
+def download_books(books_folder, images_folder):
     base_download_url = 'https://tululu.org/txt.php'
     books_ids = [i for i in range(1, 11)]
 
@@ -43,13 +71,18 @@ def download_books(folder):
             continue
 
         book_name, book_author = get_book_author(book_id)
-        download_txt(response, f'{book_id}. {book_name}', folder)
+        book_image_url = get_book_image_url(book_id)
+        image_name = get_image_name(book_image_url)
+        download_image(book_image_url, image_name, images_folder)
+        # download_txt(response, f'{book_id}. {book_name}', books_folder)
 
 
 if __name__ == '__main__':
-    folder = 'books/'
+    books_folder = 'books/'
+    images_folder = 'images/'
 
-    Path(folder).mkdir(parents=True, exist_ok=True)
-    download_books(folder)
+    Path(books_folder).mkdir(parents=True, exist_ok=True)
+    Path(images_folder).mkdir(parents=True, exist_ok=True)
+    download_books(books_folder,images_folder)
 
 
